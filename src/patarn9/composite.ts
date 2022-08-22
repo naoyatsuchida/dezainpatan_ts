93067a06c9efcc1474d04fea28dd94b9a4163d69
naoyatsuchida <naoyatsuchida9@gmail.com>
2022-08-22 21:02:40 +0900

    composit

---
+++ src/patarn9/composite.ts
@@ -0,0 +1,29 @@
+// export const composite = () => {
+//     abstract class Entry {
+//         abstract getName(): string;
+//         abstract getSize(): number;
+//         abstract printList(prefix: string): void;
+//     }
+
+
+//     class File extends Entry{
+//         constructor(private name: string, private size: number) {
+//             super();
+//         }
+
+//         getName() {
+//             return this.name;
+//     }
+
+//         getSize() {
+//             return this.size;
+//         }
+//         printList(prefix: string): void {
+
+//         }
+//     }
+
+//     class Directory extends Entry{
+
+//     }
+// }
